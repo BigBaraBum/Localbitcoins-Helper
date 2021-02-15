abd11ac1e4d21b0fc7a652c6999759943b7bcccd
Nikita Inkin <nikitainkin@yahoo.com>
2021-02-15 20:13:40 +0300

    minor changes

--- Localbitcoinshelper.user.js
+++ Localbitcoinshelper.user.js
@@ -5,6 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://localbitcoins.net/ad/*
+// @match         https://localbitcoins.net/accounts/wallet/
 
 // @grant        none
 // ==/UserScript==
@@ -13,32 +14,46 @@
   "use strict";
   let outgoingFee = 0;
   let total = 0;
-  $("<div>")
-    .appendTo("body")
-    .css({
-      position: "fixed",
-      top: "600px",
-      right: "0",
-      //width: "150px",
-      //height: "150px",
-      "font-size": "21px",
-      "background-color": "rgba(255,255,255,1)",
-      border: "3px solid blue",
-      "z-index": "99999999"
-    })
-    .addClass("my-menu")
-    .append("<p>Transaction fee: <span id='transaction-fee'></span></p>")
-    .append("<input id='first-input' type='text' placeholder='Enter first price'>")
-    .append("<input id='second-input' type='text' placeholder='Enter second price'>")
-    .append("<p>Total price btc: <span id='total-btc'></span>")
-    .append("<input id='address' placeholder='Address to watch'>")
-    .append("<p>Confirmations: <span id='confirmations'></span>")
-    .append("<p>Last blocks (in mins):</p>")
-    .append("<ul id='last-blocks'><ul>");
+  if (window.location.pathname == "/accounts/wallet/") {
+    $("#sendformBtn").click(function () {
+      let wallet = $("#div_id_address_to input").val();
+      $.ajax({
+        url: 'http://localhost:3000/wallet',
+        wallet: wallet.toString()
+      });
+    });
+  } else {
+    renderMenuTrade();
+    console.log("hi");
+  }
 
   $(window).on("load", function () {});
   getFee();
   getLastBlocks();
+  function renderMenuTrade() {
+    $("<div>")
+      .appendTo("body")
+      .css({
+        position: "fixed",
+        top: "600px",
+        right: "0",
+        //width: "150px",
+        //height: "150px",
+        "font-size": "21px",
+        "background-color": "rgba(255,255,255,1)",
+        border: "3px solid blue",
+        "z-index": "99999999"
+      })
+      .addClass("my-menu")
+      .append("<p>Transaction fee: <span id='transaction-fee'></span></p>")
+      .append("<input id='first-input' type='text' placeholder='Enter first price'>")
+      .append("<input id='second-input' type='text' placeholder='Enter second price'>")
+      .append("<p>Total price btc: <span id='total-btc'></span>")
+      .append("<input id='address' placeholder='Address to watch'>")
+      .append("<p>Confirmations: <span id='confirmations'></span>")
+      .append("<p>Last blocks (in mins):</p>")
+      .append("<ul id='last-blocks'><ul>");
+  }
   $(".my-menu input").on("input", function () {
     total = Number($("#first-input").get(0).value) + Number($("#second-input").get(0).value) + Number(outgoingFee);
     renderTotal();
